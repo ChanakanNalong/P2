90efb12f3c3e803126db6f21e896d15484efa170
anoxelazy <106942679+anoxelazy@users.noreply.github.com>
2024-10-15 20:41:46 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,176 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_login/home.dart';
-import 'login.dart';
-import 'register.dart';
+// ignore_for_file: prefer_const_constructors, override_on_non_overriding_member, unused_import
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class registerScreen extends StatefulWidget {
+  const registerScreen({Key? key}) : super(key: key);
+
+  @override
+  State<registerScreen> createState() => _RegisterScreenState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class _RegisterScreenState extends State<registerScreen> {
+  final formKey = GlobalKey<FormState>();
 
-  // This widget is the root of your application.
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+
+  Future signUp() async {
+    String url = "http://172.16.10.226/api/flutter_login/register.php";
+    final response = await http.post(Uri.parse(url), body: {
+      'name': nameController.text,
+      'password': passController.text,
+      'email': emailController.text,
+    });
+
+    var data = json.decode(response.body);
+
+    if (data['status'] == "success") {
+      Navigator.pushNamed(context, 'home');
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(data['register'])),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      backgroundColor: Color(0xFFF5F5F5),
+      body: Center(
+        child: Form(
+          key: formKey,
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'Sign Up',
+                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Please complete your',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  const Text(
+                    'biodata correctly',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  const SizedBox(height: 30),
+                  SizedBox(
+                    width: 350,
+                    child: TextFormField(
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Your name',
+                      ),
+                      validator: (val) {
+                        if (val!.isEmpty) {
+                          return 'Empty';
+                        }
+                        return null;
+                      },
+                      controller: nameController,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: 350,
+                    child: TextFormField(
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Your E-Mail',
+                      ),
+                      validator: (val) {
+                        if (val!.isEmpty) {
+                          return 'Empty';
+                        }
+                        return null;
+                      },
+                      controller: emailController,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: 350,
+                    child: TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Create your Password',
+                      ),
+                      validator: (val) {
+                        if (val!.isEmpty) {
+                          return 'Empty';
+                        }
+                        return null;
+                      },
+                      controller: passController,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: 350,
+                    child: TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Re-Type your Password',
+                      ),
+                      validator: (val) {
+                        if (val!.isEmpty) {
+                          return 'Empty';
+                        } else if (val != passController.text) {
+                          return 'Passwords do not match';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: 350,
+                    height: 60,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF3F60A0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                      onPressed: () {
+                        bool isValid = formKey.currentState!.validate();
+
+                        if (isValid) {
+                          signUp();
+                        }
+                      },
+                      child: const Text(
+                        'Sign up',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      home: login(),
-      routes: {
-        'register': (context) => register(),
-        'home':(context) => homepage(),
-        'login':(context) => login(),
-      },
     );
   }
 }
