2e1d57bdacd6621449f4188152435b35cf68b6dd
anoxelazy <106942679+anoxelazy@users.noreply.github.com>
2024-10-15 20:43:53 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,176 +1,28 @@
-// ignore_for_file: prefer_const_constructors, override_on_non_overriding_member, unused_import
-
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:flutter_login/home.dart';
+import 'login.dart';
+import 'register.dart';
 
-class registerScreen extends StatefulWidget {
-  const registerScreen({Key? key}) : super(key: key);
-
-  @override
-  State<registerScreen> createState() => _RegisterScreenState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _RegisterScreenState extends State<registerScreen> {
-  final formKey = GlobalKey<FormState>();
-
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passController = TextEditingController();
-  TextEditingController emailController = TextEditingController();
-
-  Future signUp() async {
-    String url = "http://172.16.10.226/api/flutter_login/register.php";
-    final response = await http.post(Uri.parse(url), body: {
-      'name': nameController.text,
-      'password': passController.text,
-      'email': emailController.text,
-    });
-
-    var data = json.decode(response.body);
-
-    if (data['status'] == "success") {
-      Navigator.pushNamed(context, 'home');
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(data['register'])),
-      );
-    }
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFF5F5F5),
-      body: Center(
-        child: Form(
-          key: formKey,
-          child: ListView(
-            shrinkWrap: true,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'Sign Up',
-                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    'Please complete your',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  const Text(
-                    'biodata correctly',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  const SizedBox(height: 30),
-                  SizedBox(
-                    width: 350,
-                    child: TextFormField(
-                      obscureText: false,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Your name',
-                      ),
-                      validator: (val) {
-                        if (val!.isEmpty) {
-                          return 'Empty';
-                        }
-                        return null;
-                      },
-                      controller: nameController,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    width: 350,
-                    child: TextFormField(
-                      obscureText: false,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Your E-Mail',
-                      ),
-                      validator: (val) {
-                        if (val!.isEmpty) {
-                          return 'Empty';
-                        }
-                        return null;
-                      },
-                      controller: emailController,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    width: 350,
-                    child: TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Create your Password',
-                      ),
-                      validator: (val) {
-                        if (val!.isEmpty) {
-                          return 'Empty';
-                        }
-                        return null;
-                      },
-                      controller: passController,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    width: 350,
-                    child: TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Re-Type your Password',
-                      ),
-                      validator: (val) {
-                        if (val!.isEmpty) {
-                          return 'Empty';
-                        } else if (val != passController.text) {
-                          return 'Passwords do not match';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    width: 350,
-                    height: 60,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF3F60A0),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                      ),
-                      onPressed: () {
-                        bool isValid = formKey.currentState!.validate();
-
-                        if (isValid) {
-                          signUp();
-                        }
-                      },
-                      child: const Text(
-                        'Sign up',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: login(),
+      routes: {
+        'register': (context) => registerScreen(),
+        'home':(context) => homepage(),
+        'login':(context) => login(),
+      },
     );
   }
 }
