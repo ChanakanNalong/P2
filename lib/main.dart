5cc48894a71534af80cb5994bac7505e521b4c6d
thirdpartyy <idsntn.thirdperson@gmail.com>
2024-10-19 00:51:12 +0700

    final for persent

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,54 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'HomePage/menu_screen.dart';
+import 'package:workmanager/workmanager.dart';
+import 'ParkingPage/cam_detect.dart';
+import 'yolo_service.dart';
+import 'package:http/http.dart' as http;
+
+Future<void> sendToDatabase(data) async {
+  String url = "http://-your ip-/api/flutter_login/yolo_service.php";
+  await http.post(
+    Uri.parse(url),
+      headers: {
+        "Content-Type": "application/json"
+      }, 
+      body: json.encode(data));
+}
+
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) async {
+    final yoloService = YoloWebSocketService();
+
+    // เรียกใช้ใน initState หรือฟังก์ชัน setup อื่นๆ
+    await yoloService.init('ws://172.16.10.249:81');
+
+    // เรียกใช้งาน YoloWebSocket และส่งข้อมูลขึ้นฐานข้อมูล
+    await yoloService.startDetection();
+
+    // จำนวนรถที่ detect ได้
+    int numberOfCars = yoloService.yoloResults.length;
+
+    // ส่งข้อมูลไปยังฐานข้อมูล (สมมติว่ามีฟังก์ชัน sendToDatabase)
+    await sendToDatabase(numberOfCars);
+
+    // หยุดการตรวจจับ
+    await yoloService.stopDetection();
+
+    // ปิดบริการเมื่อไม่ใช้งานแล้ว
+    await yoloService.dispose();
+
+    return Future.value(true);
+  });
+}
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
+  Workmanager().registerPeriodicTask(
+    "1",
+    "yoloWebSocketDetection",
+    frequency: const Duration(hours: 1),
+  );
   runApp(const MyApp());
 }
 
@@ -13,11 +60,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'First Flutter App',
+      title: 'SSP',
       // home: FirstScreen(),
       initialRoute: '/menu', // สามารถใช้ home แทนได้
       routes: {
-        '/menu': (context) => const MenuScreen(),
+        '/menu': (context) => YoloWebSocket(),
       },
     );
   }
